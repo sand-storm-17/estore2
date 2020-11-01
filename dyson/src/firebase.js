9cb1a782ac95bed25a2f301d23eaeae57981f479
Devansh Jyoti <devansh.jyoti8888@gmail.com>
2020-11-02 02:11:35 +0530

    First Commit, 27:00

---
+++ dyson/src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyByld8mwr-AXU9g-4Jm-Nt1DCBeoJWQc4U",
+    authDomain: "dyson-4a3bd.firebaseapp.com",
+    databaseURL: "https://dyson-4a3bd.firebaseio.com",
+    projectId: "dyson-4a3bd",
+    storageBucket: "dyson-4a3bd.appspot.com",
+    messagingSenderId: "136347244640",
+    appId: "1:136347244640:web:5b76f72f710b8f15dfcb05",
+    measurementId: "G-DMZBY99ZFM"
+  };
